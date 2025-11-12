f9e6876e4aafd9827a4c53a65797b086d0201f15
art-fatal <andrianirijanyavo@gmail.com>
2025-11-12 16:48:21 +0100

    Added Tailwind CSS configuration to the project.

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="font-bold">test</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/ui']
+  css: ['./app/assets/css/main.css'],
+  modules: ['@nuxt/ui'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
